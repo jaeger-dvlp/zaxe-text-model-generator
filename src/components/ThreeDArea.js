02d7c5632b88601917f6fc8f00edb7f9c1a803bf
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-09-08 18:21:19 +0300

    Create text and apply to keychain.

--- src/components/ThreeDArea.js
+++ src/components/ThreeDArea.js
@@ -12,16 +12,14 @@ export default class ThreeDArea extends PureComponent {
       const camera = new THREE.PerspectiveCamera()
       const scene = new THREE.Scene()
       objLoader.load('./3dmodels/keychain.obj', (obj) => {
-        //obj.position.set(0, 0, 0)
-
         scene.add(obj)
         obj.position.set(0, 30, 0)
         obj.scale.set(parseInt(name.length / 8) + 1, 1)
         obj.rotation.set(0, 0, 0)
-        //object = obj
       })
 
       const loader = new THREE.FontLoader()
+
       loader.load('./fonts/Montserrat_Regular.json', (font) => {
         const geometry = new THREE.TextGeometry(name, {
           font: font,
@@ -35,13 +33,17 @@ export default class ThreeDArea extends PureComponent {
         ])
 
         scene.add(textMesh)
+
         textMesh.position.set(-(name.length * 7) - 10, 12, 0)
         textMesh.rotation.set(0, 0, 0)
+
         renderer.render(scene, camera)
+
         let exporter = new STLExporter()
         let stl = exporter.parse(scene)
-        //const buffer = exportSTL.fromMesh(textMesh)
+
         const blob = new Blob([stl])
+
         saveAs(blob, 'test.stl')
       })
     }
