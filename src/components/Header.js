8761b8b983a794dc3104fcfec64caa481a0a0de6
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-09-13 01:20:30 +0300

    Add redirectToWeb func.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,16 @@
 import React, {Component} from 'react'
 
 export default class Header extends Component {
+  redirectToWebsite = () => {
+    window.location.href = 'https://zaxe.com'
+  }
   render() {
     return (
       <div
         data-aos="fade-down"
         className="col-12 bg-light header p-3 d-flex justify-content-center"
       >
-        <div className="logo mx-auto"></div>
+        <div className="logo mx-auto" onClick={this.redirectToWebsite}></div>
       </div>
     )
   }
